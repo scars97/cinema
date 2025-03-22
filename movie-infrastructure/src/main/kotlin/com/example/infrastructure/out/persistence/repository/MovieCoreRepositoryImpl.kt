439b57ffb481fc6362c5a9eb4108ed46258184d8
박성현 <qkr970504@gmail.com>
2025-03-22 13:24:16 +0900

    [refactor] 영화 조회 방식 변경
    - jpa 메서드 -> QueryDsl

--- movie-infrastructure/src/main/kotlin/com/example/infrastructure/out/persistence/repository/MovieCoreRepositoryImpl.kt
+++ movie-infrastructure/src/main/kotlin/com/example/infrastructure/out/persistence/repository/MovieCoreRepositoryImpl.kt
@@ -2,22 +2,39 @@ package com.example.infrastructure.out.persistence.repository
 
 import com.example.business.movie.domain.Movie
 import com.example.business.movie.repository.MovieRepository
+import com.example.infrastructure.out.persistence.entity.MovieEntity
+import com.example.infrastructure.out.persistence.entity.QMovieEntity
 import com.example.infrastructure.out.persistence.mapper.MovieMapper
-import com.example.infrastructure.out.persistence.repository.MovieJpaRepository
+import com.querydsl.core.types.Projections
+import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
 
 @Repository
 class MovieCoreRepositoryImpl(
-    private val jpaRepository: MovieJpaRepository
+    private val queryFactory: JPAQueryFactory
 ): MovieRepository {
 
     override fun getMoviesReleasedUntil(now: LocalDate): List<Movie> {
-        val movies = jpaRepository.findByReleaseDateLessThanEqualOrderByReleaseDateAsc(now)
+        val movie = QMovieEntity.movieEntity
 
-        return movies.stream()
+        return queryFactory
+            .select(
+                Projections.constructor(MovieEntity::class.java,
+                    movie.id,
+                    movie.title,
+                    movie.thumbnail,
+                    movie.releaseDate,
+                    movie.runTime,
+                    movie.genre,
+                    movie.rating
+                )
+            )
+            .from(movie)
+            .where(movie.releaseDate.loe(now))
+            .orderBy(movie.releaseDate.asc())
+            .fetch()
             .map { MovieMapper.toMovie(it) }
-            .toList()
     }
 
 }
