4433f7d09c251204684f8ece6e9f5acbfb368193
박성현 <qkr970504@gmail.com>
2025-03-27 23:30:17 +0900

    [chore] 엔티티 필드 키워드 수정
    - post 요청 시 엔티티 생성되지 않는 문제 발생으로 인한 수정

--- movie-infrastructure/build.gradle.kts
+++ movie-infrastructure/build.gradle.kts
@@ -3,9 +3,7 @@ dependencies {
     implementation(project(":movie-business"))
     implementation(project(":movie-common"))
 
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
 
@@ -13,5 +11,6 @@ dependencies {
     implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
 
+    // Mysql
     runtimeOnly("com.mysql:mysql-connector-j")
 }
