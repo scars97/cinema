dc035fb9b80bd600db21b968728af533f9b0889d
박성현 <qkr970504@gmail.com>
2025-07-29 16:31:11 +0900

    [refactor] Lua script 구현체 수정

--- movie-common/src/main/kotlin/com/example/common/config/RedisConfig.kt
+++ movie-common/src/main/kotlin/com/example/common/config/RedisConfig.kt
@@ -1,5 +1,6 @@
 package com.example.common.config
 
+import com.example.common.model.RateLimitResponse
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
@@ -55,9 +56,9 @@ class RedisConfig(
     }
 
     @Bean
-    fun rateLimitScript(): RedisScript<Boolean> {
+    fun rateLimitScript(): RedisScript<List<*>> {
         val script = ClassPathResource("luascript/ratelimit.lua")
-        return RedisScript.of(script, Boolean::class.java)
+        return RedisScript.of(script, List::class.java)
     }
 
     @Bean
--- movie-common/src/main/kotlin/com/example/common/ratelimit/RedisLuaRateLimiter.kt
+++ movie-common/src/main/kotlin/com/example/common/ratelimit/RedisLuaRateLimiter.kt
@@ -1,6 +1,7 @@
 package com.example.common.ratelimit
 
 import com.example.common.annotation.LimitRequestPerTime
+import com.example.common.model.RateLimitResponse
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.core.script.RedisScript
 import org.springframework.stereotype.Component
@@ -8,16 +9,22 @@ import java.util.concurrent.TimeUnit
 
 @Component("RedisLuaRateLimiter")
 class RedisLuaRateLimiter(
-    private val rateLimitScript: RedisScript<Boolean>,
+    private val rateLimitScript: RedisScript<List<*>>,
     private val redisTemplate: RedisTemplate<String, Any>
 ): RateLimiter {
 
-    override fun tryCall(key: String, limitRequestPerTime: LimitRequestPerTime): Boolean {
-        return redisTemplate.execute(
+    override fun tryCall(key: String, limitRequestPerTime: LimitRequestPerTime): RateLimitResponse {
+        val result = redisTemplate.execute(
             rateLimitScript,
             listOf(key),
             limitRequestPerTime.limitCount,
             TimeUnit.SECONDS.convert(limitRequestPerTime.ttl, limitRequestPerTime.ttlTimeUnit)
+        )
+
+        return RateLimitResponse(
+            allowed = (result[0] as Long) == 1L,
+            limit = (result[1] as Long),
+            remaining = (result[2] as Long)
         )
     }
 
